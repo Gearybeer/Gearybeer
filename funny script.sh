baadcb7b4c271be710ad802ac4ee90661db91dc9
Gearybeer <momur7489@gmail.com>
2025-05-03 22:15:55 -0500

    Add files via upload

---
+++ funny script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ASCII Art message
+echo "########################################"
+echo "#                                      #"
+echo "#    Hey... Why do you care to do this  #"
+echo "#    lmfao                             #"
+echo "#                                      #"
+echo "########################################"
+
+# Plaintext in pink
+echo -e "\033[38;5;213mHey.. uh.. im mostly a music producer but i dabble in coding."
+echo -e "\033[38;5;213mmostly in lua. thanks for visiting my page i guess.\033[0m"
